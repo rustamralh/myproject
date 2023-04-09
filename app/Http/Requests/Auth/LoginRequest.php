2f4a8d395d07958fc70770b8cf4e4d36f7223222
RRalh <rustam@xyrintech.com>
2023-04-09 17:21:54 +0530

    Added throttle timeout.

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Requests\Auth;
 
-use Illuminate\Cache\RateLimiter;
+use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
+     *
+     * @return bool
      */
-    public function authorize(): bool
+    public function authorize()
     {
         return true;
     }
@@ -19,40 +24,94 @@ class LoginRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     * @return array
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            'email'=>[
-                'required',
-                'email'
-            ],
-            'password'=>[
-                'required',
-            ],
+            'email'    => 'required|string|email',
+            'password' => 'required|string',
         ];
     }
 
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function authenticate()
     {
-        if (Auth::attempt(['email'=>strtolower($this->email), 'password'=>$this->password])) {
-            return  true;
-        } else {
-            return back()->withErrors([
-                'email' => 'The provided credentials do not match our records.',
-            ])->onlyInput('email');
+        $this->ensureIsNotRateLimited();
+
+        if (! Auth::attempt([
+            'email'    => strtolower($this->email),
+            'password' => $this->password,
+        ], $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
         }
+
+        RateLimiter::clear($this->throttleKey());
     }
 
+    // public function ensureUserIsAdmin()
+    // {
+    //     $isTenantOwner = Tenant::whereHas('user', function ($query) {
+    //         $query->where('email', Str::lower($this->email));
+    //     })
+    //     ->exists();
+
+    //     $isAdmin = TenantUser::whereHas('user', function ($query) {
+    //         $query->where('email', Str::lower($this->email));
+    //     })
+    //     ->where('is_admin', true)
+    //     ->exists();
+
+    //     if ($isTenantOwner || $isAdmin) {
+    //     } else {
+    //         throw ValidationException::withMessages([
+    //             'email' => __('auth.admin'),
+    //         ]);
+    //     }
+    // }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function ensureIsNotRateLimited()
     {
         if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
-            return  true;
-        } else {
-            return back()->withErrors([
-                'email' => 'The provided credentials do not match our records.',
-            ])->onlyInput('email');
+            return;
         }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     *
+     * @return string
+     */
+    public function throttleKey()
+    {
+        return Str::lower($this->input('email')).'|'.$this->ip();
     }
 }
